fb873d5d0ba517a4c5f394318847154aa58a1bda
Arjun Satwani <80617915+arjunsatwani@users.noreply.github.com>
2021-07-04 04:30:52 +0530

    Update Tkinter OX (Minified).py

--- Tkinter OX (Minified).py
+++ Tkinter OX (Minified).py
@@ -1,118 +1,119 @@
-s='|'
-r='_'
-l=None
-T='O'
-S='X'
-O='normal'
-I='disable'
-G='white'
-F='black'
-E=' '
-D='bold'
-C='Helvetica'
-from tkinter import Tk,Label as K,Button as J
+from tkinter import Tk,Label as O,Button as I
+import random as u
+k=None
+X='O'
+W='X'
+T=''
+N='normal'
+K='white'
+H='disable'
+F='bold'
+E='Helvetica'
+D=' '
+C='black'
 B=Tk()
-B.config(bg=F)
-H=l
-L=l
-A=[E,E,E,E,E,E,E,E,E]
-U=0
-V=0
-W=0
-X=0
-def t():
-	C='';global L,H,A,U,V,W,X;W+=1
-	if E not in A:X+=1;q.config(text=f"TIE : {X}")
-	if L==S:U+=1;n.config(text=f"X : {U}")
-	elif L==T:V+=1;o.config(text=f"O : {V}")
-	for B in h:B.config(state=I)
-	j.config(state=O),k.config(state=O),N.config(text=C),Q.config(text=C,width=0,bg=F),R.config(text=C,bg=F,width=0,height=0),p.config(text=f"TOTAL : {W}");L=l;H=l;D=-1
-	for B in A:D+=1;A[D]=E
-	for B in h:B.config(text=C)
-	P.config(state=I)
-def i():pass
-def u():
-	B=A[0]==A[1]==A[2]!=E;C=A[3]==A[4]==A[5]!=E;D=A[6]==A[7]==A[8]!=E
-	if B:Q.place(x=50,y=630);return A[0]
-	elif C:Q.place(x=50,y=950);return A[3]
-	elif D:Q.place(x=50,y=1270);return A[6]
+B.config(bg=C)
+G=k
+J=k
+A=[D,D,D,D,D,D,D,D,D]
+P=0
+Q=0
+R=0
+S=0
+t=[W,X]
 def v():
-	B=A[0]==A[3]==A[6]!=E;C=A[1]==A[4]==A[7]!=E;D=A[2]==A[5]==A[8]!=E
-	if B:R.place(x=175,y=535);return A[0]
-	elif C:R.place(x=535,y=535);return A[1]
-	elif D:R.place(x=885,y=535);return A[2]
+	global J,G,A,P,Q,R,S,t;F=u.choice(t);R+=1;G=F;M.config(text=G)
+	if D not in A:S+=1;s.config(text=f"TIE : {S}")
+	if J==W:P+=1;p.config(text=f"X : {P}")
+	elif J==X:Q+=1;q.config(text=f"O : {Q}")
+	M.config(text=T),U.config(text=T,width=0,bg=C),V.config(text=T,bg=C,width=0,height=0),r.config(text=f"TOTAL : {R}");J=k;m(G);B=-1
+	for E in A:B+=1;A[B]=D
+	for E in Z:E.config(text=T)
+	Y.config(state=H)
 def w():
-	B=A[0]==A[4]==A[8]!=E;C=A[2]==A[4]==A[6]!=E
+	global J,G,A,P,Q,R,S;R=0;S=0;s.config(text=f"TIE : {S}");P=0;p.config(text=f"X : {P}");Q=0;q.config(text=f"O : {Q}")
+	for B in Z:B.config(state=H)
+	n.config(state=N),o.config(state=N),M.config(text=T),U.config(text=T,width=0,bg=C),V.config(text=T,bg=C,width=0,height=0),r.config(text=f"TOTAL : {R}");J=k;G=k;E=-1
+	for B in A:E+=1;A[E]=D
+	for B in Z:B.config(text=T)
+	a.config(state=H);Y.config(state=H)
+def l():pass
+def x():
+	B='s';C=A[0]==A[1]==A[2]!=D;E=A[3]==A[4]==A[5]!=D;F=A[6]==A[7]==A[8]!=D
+	if C:U.grid(row=3,sticky=B,columnspan=3,rowspan=2,pady=140);return A[0]
+	elif E:U.grid(row=3,sticky=B,columnspan=3,rowspan=3,pady=140);return A[3]
+	elif F:U.grid(row=3,sticky=B,columnspan=3,rowspan=4,pady=140);return A[6]
+def y():
+	B='e';C=A[0]==A[3]==A[6]!=D;E=A[1]==A[4]==A[7]!=D;F=A[2]==A[5]==A[8]!=D
+	if C:V.grid(row=4,sticky=B,rowspan=3,padx=170,columnspan=1);return A[0]
+	elif E:V.grid(row=4,sticky=B,rowspan=3,padx=173,columnspan=2);return A[1]
+	elif F:V.grid(row=4,sticky=B,rowspan=3,padx=173,columnspan=3);return A[2]
+def z():
+	B=A[0]==A[4]==A[8]!=D;C=A[2]==A[4]==A[6]!=D
 	if B:return A[0]
 	elif C:return A[2]
-def x():
-	global L;A=u();B=v();C=w()
-	if A:Q.config(text=r,width=320,bg=G);L=A
-	elif B:R.config(text=s,bg=G,width=3,height=120);L=B
-	elif C:L=C
-def M(Button,Position):
-	C=Position;B=Button;global H
-	if H==S:
-		if A[C]==E:A[C]=H;B.config(command=i,state=I)
-		B.config(text=H,fg='yellow',state=O);H=T
-	elif H==T:
-		if A[C]==E:A[C]=H;B.config(command=i,state=I)
-		B.config(text=H,fg='orange',state=O);H=S
-	N.config(text=f"{H}'s Turn")
-	if E not in A:N.config(text="It's A Tie");P.config(state=O);P.place(x=415,y=1500)
-	x()
-	if L==S or L==T:
-		N.config(text=f"{L} WON ");P.config(state=O)
-		for D in h:D.config(command=i)
-		P.place(x=415,y=1500)
+def A0():
+	global J;A=x();B=y();C=z()
+	if A:U.config(text='_',width=330,bg=K);J=A
+	elif B:V.config(text='|',bg=K,width=3,height=116);J=B
+	elif C:J=C
+def L(Button,Position):
+	C=Position;B=Button;global G
+	if G==W:
+		if A[C]==D:A[C]=G;B.config(command=l,state=H)
+		B.config(text=G,fg='yellow',state=N);G=X
+	elif G==X:
+		if A[C]==D:A[C]=G;B.config(command=l,state=H)
+		B.config(text=G,fg='orange',state=N);G=W
+	M.config(text=f"{G}'s Turn")
+	if D not in A:M.config(text="It's A Tie");a.config(state=N);Y.config(state=N)
+	A0()
+	if J==W or J==X:
+		M.config(text=f"{J} WON ");a.config(state=N);Y.config(state=N)
+		for E in Z:E.config(command=l)
 def m(Player):
-	global H
-	for A in h:A.config(state=O)
-	j.config(state=I),k.config(state=I);H=Player;N.config(text=f"{H}'s Turn");Y.config(command=lambda:M(Y,0)),Z.config(command=lambda:M(Z,1)),a.config(command=lambda:M(a,2)),b.config(command=lambda:M(b,3)),c.config(command=lambda:M(c,4)),d.config(command=lambda:M(d,5)),e.config(command=lambda:M(e,6)),f.config(command=lambda:M(f,7)),g.config(command=lambda:M(g,8))
-j=J(B,text=T,bg=F,fg=G,font=(C,9,D),bd=10,command=lambda:m(T))
-k=J(B,text=S,bg=F,fg=G,font=(C,9,D),bd=10,command=lambda:m(S))
-y=J(B,text='EXIT',bg=F,fg=G,font=(C,5,D),bd=10,command=B.destroy)
-P=J(B,text='RESTART',bg=F,fg=G,font=(C,5,D),bd=10,command=t)
-Y=J(B,bg=F,width=2,font=(C,40,D),state=I)
-Z=J(B,bg=F,width=2,font=(C,40,D),state=I)
-a=J(B,bg=F,width=2,font=(C,40,D),state=I)
-b=J(B,bg=F,width=2,font=(C,40,D),state=I)
-c=J(B,bg=F,width=2,font=(C,40,D),state=I)
-d=J(B,bg=F,width=2,font=(C,40,D),state=I)
-e=J(B,bg=F,width=2,font=(C,40,D),state=I)
-f=J(B,bg=F,width=2,font=(C,40,D),state=I)
-g=J(B,bg=F,width=2,font=(C,40,D),state=I)
-z=K(B,text=s,fg=G,bg=G,font=(C,1,D),height=134)
-A0=K(B,text=s,fg=G,bg=G,font=(C,1,D),height=134)
-A1=K(B,text=r,fg=G,bg=G,font=(C,1,D),width=355)
-A2=K(B,text=r,fg=G,bg=G,font=(C,1,D),width=355)
-N=K(B,fg=G,bg=F,font=(C,15,D))
-Q=K(B,font=(C,1,D))
-R=K(B,font=(C,1,D))
-n=K(B,text=f"X : {U}",fg=G,bg=F,font=(C,10,D))
-o=K(B,text=f"O : {V}",fg=G,bg=F,font=(C,10,D))
-p=K(B,text=f"TOTAL : {W}",fg=G,bg=F,font=(C,10,D))
-q=K(B,text=f"TIE : {X}",fg=G,bg=F,font=(C,10,D))
-h=[Y,Z,a,b,c,d,e,f,g]
-j.place(x=645,y=1640)
-k.place(x=278,y=1640)
-y.place(x=445,y=1800)
-Y.place(y=490)
-Z.place(x=360,y=490)
-a.place(x=710,y=490)
-b.place(y=810)
-c.place(x=360,y=810)
-d.place(x=710,y=810)
-e.place(y=1130)
-f.place(x=360,y=1130)
-g.place(x=710,y=1130)
-z.place(x=355,y=490)
-A0.place(x=710,y=490)
-A1.place(y=795)
-A2.place(y=1115)
-N.place(x=370,y=200)
-n.place(x=300,y=350)
-o.place(x=650,y=350)
-p.pack()
-q.pack()
+	global G
+	for A in Z:A.config(state=N)
+	n.config(state=H),o.config(state=H);G=Player;M.config(text=f"{G}'s Turn");b.config(command=lambda:L(b,0)),c.config(command=lambda:L(c,1)),d.config(command=lambda:L(d,2)),e.config(command=lambda:L(e,3)),f.config(command=lambda:L(f,4)),g.config(command=lambda:L(g,5)),h.config(command=lambda:L(h,6)),i.config(command=lambda:L(i,7)),j.config(command=lambda:L(j,8))
+n=I(B,text=X,bg=C,fg=K,font=(E,9,F),bd=15,command=lambda:m(X))
+o=I(B,text=W,bg=C,fg=K,font=(E,9,F),bd=15,command=lambda:m(W))
+A1=I(B,text='EXIT',bg=C,fg=K,font=(E,5,F),bd=15,command=B.destroy)
+Y=I(B,text='REMATCH',bg=C,fg=K,font=(E,5,F),bd=15,command=v,state=H)
+a=I(B,text='RESTART',bg=C,fg=K,font=(E,5,F),bd=15,command=w,state=H)
+b=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+c=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+d=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+e=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+f=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+g=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+h=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+i=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+j=I(B,bg=C,width=2,font=(E,30,F),state=H,bd=30)
+Z=[b,c,d,e,f,g,h,i,j]
+M=O(B,fg=K,bg=C,font=(E,15,F))
+U=O(B,font=(E,1,F))
+V=O(B,font=(E,1,F))
+p=O(B,text=f"X : {P}",fg=K,bg=C,font=(E,10,F))
+q=O(B,text=f"O : {Q}",fg=K,bg=C,font=(E,10,F))
+r=O(B,text=f"TOTAL : {R}",fg=K,bg=C,font=(E,10,F))
+s=O(B,text=f"TIE : {S}",fg=K,bg=C,font=(E,10,F))
+r.grid(column=1)
+s.grid(column=1)
+M.grid(column=1,pady=70)
+p.grid(columnspan=2)
+q.grid(row=3,column=1,columnspan=2)
+b.grid(row=4)
+c.grid(row=4,column=1)
+d.grid(row=4,column=2)
+e.grid(row=5)
+f.grid(row=5,column=1)
+g.grid(row=5,column=2)
+h.grid(row=6)
+i.grid(row=6,column=1)
+j.grid(row=6,column=2)
+Y.grid(row=8,ipadx=50,pady=50)
+a.grid(row=8,column=2,ipadx=50)
+o.grid(row=10,columnspan=2,ipadx=50)
+n.grid(row=10,column=1,columnspan=2,ipadx=50)
+A1.grid(column=1,pady=50,ipadx=50)
 B.mainloop()
